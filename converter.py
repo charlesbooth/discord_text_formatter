89cd91d6431c653fb44bc0ea019235e15503b80e
charlesbooth <bharlescooth@gmail.com>
2023-02-19 20:21:51 -0600

    Made final dist with GUI

---
+++ converter.py
@@ -0,0 +1,84 @@
+import tkinter as tk
+import pyperclip
+
+from docx import Document
+from string import punctuation
+from tkinter import filedialog
+from tkinter import messagebox
+
+
+def make_modifiers(r):
+    form = lambda x, b: x if b else ''
+    check = \
+        (
+            form('*' , any\
+                    ((r.italic, r.style.name == 'Emphasis'))),
+            form\
+                ('**', \
+                    r.bold),
+            form\
+                ('__', \
+                    r.underline)
+         )
+    return ''.join(check)
+
+
+def create_text(x, c):
+    string = '%s'*3 % \
+        (f'{c}', x.strip(), f'{c[::-1]} ')
+    return string
+
+
+def check_paragraphs(doc):
+    base = str()
+    for para in doc.paragraphs:
+        new = check_runs(para.runs)
+        base += new + '\n'
+    return base
+
+
+def check_runs(runs, new=str()):
+    for r in runs:
+        if r.text.isspace():
+            continue
+        if r.text in punctuation:
+            new = new[:-1] + r.text
+            continue
+        mod = make_modifiers(r)
+        new += create_text(r.text, mod)
+    return new
+
+
+def main():
+    path = filedialog.askopenfilename()
+    doc = Document(path)
+    text = check_paragraphs(doc)
+
+    window = tk.Tk()
+    window.title('Output')
+    window.geometry('650x250')
+    
+    output = tk.Label(
+                        window,
+                        text=text, 
+                        font=(14), 
+                        wraplength=600, 
+                        justify='left'
+                     )
+    output.grid\
+        (row=0, column=0, padx=5, pady=5)
+
+    copy = tk.Button\
+        (
+            window, 
+            text='Copy to Clipboard', 
+            command=pyperclip.copy(text)
+        )
+    copy.grid\
+        (row=2, column=0, padx=5, pady=5)
+        
+    window.mainloop()
+
+
+if __name__ == '__main__':
+    main()
--- testing/converter_test.py
+++ testing/converter_test.py
@@ -1,56 +1,104 @@
+import tkinter as tk
+import pyperclip
+
 from docx import Document
 from string import punctuation
+from tkinter import filedialog
+from tkinter import messagebox
 
 
-def detect_format(r):
-    check = r.bold, r.italic, r.underline
-    return {'b': r.bold,
-            'i': r.italic,
-            'u': r.underline}
-
-
-def check_run(r):
+def make_modifiers(r):
     form = lambda x, b: x if b else ''
     check = \
-        (form('*' , r.italic),
-         form('**', r.bold),
-         form('__', r.underline))
+        (
+            form('*' , any\
+                    ((r.italic, r.style.name == 'Emphasis'))),
+            form\
+                ('**', \
+                    r.bold),
+            form\
+                ('__', \
+                    r.underline)
+         )
     return ''.join(check)
 
 
 def create_text(x, c):
     string = '%s'*3 % \
-        (c, x.strip(), c)
-    # if x[0].isspace():
-    #     return ' ' + string
-    # if x[-1].isspace():
-    #     return string + ' '
-    return string + ' '
+        (f'{c}', x.strip(), f'{c[::-1]} ')
+    return string
+
+
+# def check_paragraphs(doc):
+#     base = []
+#     for para in doc.paragraphs:
+#         new = check_runs(para.runs)
+#         base.append(new)
+#     return base
 
 
 def check_paragraphs(doc):
-    base = []
+    base = str()
     for para in doc.paragraphs:
-        new = str()
-        for r in para.runs:
-            print(r.text)
-            if r.text.isspace():
-                continue
-            if r.text in punctuation:
-                print('here')
-                new = new[:-1] + r.text
-                continue
-            mod = check_run(r)
-            new += create_text(r.text, mod)     
-        base.append(new)
+        new = check_runs(para.runs)
+        base += new + '\n'
     return base
 
 
+def check_runs(runs, new=str()):
+    for r in runs:
+        if r.text.isspace():
+            continue
+        if r.text in punctuation:
+            new = new[:-1] + r.text
+            continue
+        mod = make_modifiers(r)
+        new += create_text(r.text, mod)
+    return new
+
+
 def main():
-    #doc = Document('converter\\example_doc.docx')
-    doc = Document('testing\\test_doc.docx')
-    for i in check_paragraphs(doc):
-        print(i)
+    path = filedialog.askopenfilename()
+    doc = Document(path)
+    text = check_paragraphs(doc)
+
+    window = tk.Tk()
+    window.title('Output')
+    window.geometry('650x250')
+    output = tk.Label(
+                        window,
+                        text=text, 
+                        font=(14), 
+                        wraplength=600, 
+                        justify='left'
+                     )
+    output.grid\
+        (row=0, column=0, padx=5, pady=5)
+    quit = tk.Button\
+        (
+            window, 
+            text='Quit', 
+            command=window.destroy
+        )
+    quit.grid\
+        (row=1, column=0, padx=5, pady=5)
+    copy = tk.Button\
+        (
+            window, 
+            text='Copy to Clipboard', 
+            command=pyperclip.copy(text)
+        )
+    copy.grid\
+        (row=2, column=0, padx=5, pady=5)
+        
+    window.mainloop()
+
+    
+    
+    # print('-', ' -', '  -', sep='\n')
+    # print(check_paragraphs(doc))
+    # print('  -', ' -', '-', sep='\n')
+    #pyperclip.copy()
 
 
 if __name__ == '__main__':
